80de4bfd8ba325a192792ec4bb63562354c534c6
Abdul Rauf <social@webwidely.com>
2024-10-14 18:17:48 +0500

    whatsapp icon updated

--- src/components/global/WhatsApp.js
+++ src/components/global/WhatsApp.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-import Siteicon from "../../images/SiteIcon.png"
+import { FaWhatsapp } from 'react-icons/fa'
 
 const WhatsApp = () => {
   return (
-    <div>
-      {/* Other page content */}
-      
-      {/* WhatsApp Widget */}
-      <FloatingWhatsApp 
-        phoneNumber="+14039039110" 
-        message="Hello! How can we help you?"
-        style={{ zIndex: 999 }}
-        accountName="Dr Talib Muhammed"
-        avatar={Siteicon}
-      />
+    <div className="fixed bottom-0 right-0 m-4 z-50">
+      <a href="https://wa.me/+14039039110?text=Hello! How can we help you?" target="_blank" rel="noopener noreferrer">
+        <FaWhatsapp className="text-white text-6xl bg-green-500 p-2 rounded-full " style={{ boxShadow: '0 0 8px #00ff00' }} />
+      </a>
     </div>
   );
 };
