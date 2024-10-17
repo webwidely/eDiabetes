fdb710b0b77ddd4117143d6318d206260fe6dc37
Abdul Rauf <social@webwidely.com>
2024-10-17 17:29:45 +0500

    google analytics is added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,7 +10,6 @@ import "./src/styles/global.css"
 // gatsby-browser.js
 import Lenis from "@studio-freight/lenis"
 
-
 let lenis
 
 export const onInitialClientRender = () => {
@@ -26,4 +25,16 @@ export const onInitialClientRender = () => {
   }
 
   requestAnimationFrame(raf)
+
+  // Adding Google Tag Manager script
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-M9TG3HJC9V';
+  document.head.appendChild(script);
+
+  // Adding Google Tag Manager configuration
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-M9TG3HJC9V');
 }
